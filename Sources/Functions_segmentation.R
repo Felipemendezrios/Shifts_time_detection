b49f65f3d170e06e5b837125edafd5afb019252a
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-02-10 09:59:58 +0100

    Create funtion to estimate initial guess of prior information of tau

--- Sources/Functions_segmentation.R
+++ Sources/Functions_segmentation.R
@@ -2,7 +2,35 @@
 # Module : Function of segmentation ------
 #-----------------------------------------
 
-prior_ini_guess_tau	<- function(xp, nS) {
-  
+#============================================
+# Initial guess to prior information of tau
+#============================================
+
+prior_ini_guess_tau	<- function(XP,nS) {
+  XP_half=0
+    for (i in 1:length(XP)) {
+      XP_half[j] = (XP[i+1]+XP[i])/2
+    }
+    if ((length(XP)-nS) <= 2) {
+      tstart=0
+      for (i in 1: (nS-1)) {
+        tstart[i] = XP_half[i]
+      }
+    } else {
+      step = trunc((length(XP)-1)/(nS-1), digits = 0)
+      init = trunc((length(XP)-1)/2)
+      tstart=0
+      aaa = 0
+      for (i in 1: (nS-1)) {
+        if (i %% 2 == 0){
+          tstart[i] = XP_half[init + step*aaa]
+        } else {
+          tstart[i] = XP_half[init - step*aaa]
+          aaa=aaa+1
+        }
+      }
+      tstart =sort(tstart)
+    }
+  return(tstart)
 }
 
