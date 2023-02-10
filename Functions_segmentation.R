49c68c8c194b74558a2e42db3ed5fdba352c5c5f
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-02-10 08:56:39 +0100

    Create scritp with only funtion of segmentation

---
+++ Functions_segmentation.R
@@ -0,0 +1,8 @@
+#-----------------------------------------
+# Module : Function of segmentation ------
+#-----------------------------------------
+
+prior_ini_guess_tau	<- function(xp, nS) {
+  
+}
+
